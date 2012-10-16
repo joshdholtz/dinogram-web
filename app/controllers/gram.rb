71f5e22599e3cfed23d9b835aca558bccd10c17d
Josh Holtz <josh@rokkincat.com>
2012-10-16 16:27:56 -0500

    Commititng file upload

--- app/controllers/gram.rb
+++ app/controllers/gram.rb
@@ -10,4 +10,20 @@ Dinogram.controllers :gram do
 		render 'gram/gram'
 	end
 
+	post "/" do
+		unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
+			return haml(:upload)
+		end
+
+		while blk = tmpfile.read(65536)
+			AWS::S3::Base.establish_connection!(
+			:access_key_id     => ENV['S3_KEY'],
+			:secret_access_key => ENV['S3_SECRET'])
+			AWS::S3::S3Object.store(name,open(tmpfile),ENV['S3_BUCKET'],:access => :public_read)     
+		end
+
+		'success'
+
+	end
+
 end
